4283c88fc73d76fa7cb6f2e245e0ecde0de874ea
elvernn <ikrarramadaniadittya@gmail.com>
2023-11-26 21:03:20 +0700

    ikrar

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,10 @@
+<div class="navbar bg-base-100 shadow-lg">
+    <div class="flex-1">
+      <a class="btn btn-ghost text-xl">IRA EMPLOYEES</a>
+    </div>
+    <div class="flex-none">
+      <ul class="menu menu-horizontal px-1">
+        <li><a href="/employee">Home</a></li>
+      </ul>
+    </div>
+  </div>
